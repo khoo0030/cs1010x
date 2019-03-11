18e1d5a004d435e982128879f7ba028925cae35e
Gerald Khoo <khoo0030@e.ntu.edu.sg>
2019-03-11 23:57:34 +0800

    Update tutorial04

--- tutorials/tutorial04.py
+++ tutorials/tutorial04.py
@@ -71,11 +71,136 @@ def sum(term, a, next, b):
     base = 0
     return accumulate(combiner, base, term, a, next, b)
 
-print(sum(lambda x: x*2, 1, lambda x: x+1, 5))
-print(sum(lambda x: x*2, 0, lambda x: x+2, 10))
-print(sum(lambda x: x**2, 1, lambda x: x+1, 5))
+#print(sum(lambda x: x*2, 1, lambda x: x+1, 5))
+#print(sum(lambda x: x*2, 0, lambda x: x+2, 10))
+#print(sum(lambda x: x**2, 1, lambda x: x+1, 5))
 
 
 #Q5:
-
+def accumulate_iter2(combiner, null_value, term, a, next, b):
+    if a > b:
+        return null_value
     
+    result = 0
+
+    step = next(0)
+    #print("step: " + str(step))
+
+    for i in range(b, a-1, -step):
+        #print("i: " + str(i) + "; a: " + str(a) + "; b: " + str(b))
+        if i == b:
+            result = combiner(null_value, term(i))
+        else:
+            result = combiner(result, term(i))
+        
+    return result
+
+def accumulate_iter3(combiner, null_value, term, a, next, b):
+    if a > b:
+        return null_value
+    
+    result = null_value
+
+    next_a = a
+
+    while(next_a <= b):
+        print("next_a value: " + str(next_a))
+        result = combiner(result, term(next_a))
+        next_a = next(next_a)
+
+    return result
+
+# has to be this order: (term(1), (term(2), (term(3), null_value)))
+# not this: (((null_value, term(1)), term(2)), term(3))
+
+def accumulate_iter(combiner, null_value, term, a, next, b):
+    if a > b:
+        return null_value
+
+    result = 0
+
+    step = next(0)
+    #print("step: " + str(step))
+
+    for i in range(b, a-1, -step):
+        print("i: " + str(i) + "; a: " + str(a) + "; b: " + str(b))
+        if i == b:
+            print("i == b")
+            term_value = term(i)
+            print("term value: " + str(term_value))
+            
+            result = combiner(term(i), null_value)
+            print("result value: " + str(result))
+            print("----------")
+        else:
+            print("i != b")
+            term_value = term(i)
+            print("term value: " + str(term_value))
+            
+            result = combiner(term(i), result)
+            print("result value: " + str(result))
+            print("----------")
+        
+    return result
+        
+print(accumulate_iter(lambda x,y: x+y, 1, lambda x: x*x, 1, lambda x: x+1, 5))        
+#print(accumulate_iter(lambda x,y: x+y, 1, lambda x: x*x, 1, lambda x: x+1, 5))
+#print(accumulate_iter(lambda x,y: x+y, 0, lambda x: x*x, 1, lambda x: x+1, 5))
+
+
+#Q6:
+def make_point(x, y):
+    return (x,y)
+
+def x_point(p):
+    return p[0]
+    
+def y_point(p):
+    return p[1]
+
+#For running public test case, do not delete
+#p1 = make_point(2, 3)
+
+#Q7:
+def make_segment(p1, p2):
+    return (p1, p2)
+    
+def start_segment(s):
+    return s[0]
+
+def end_segment(s):
+    return s[1]
+    
+#do not uncomment! this is for reference only.
+p1 = make_point(2, 3)
+p2 = make_point(5, 7)
+
+#print(x_point(start_segment(make_segment(p1, p2))))
+#print(y_point(start_segment(make_segment(p1, p2))))
+#print(x_point(end_segment(make_segment(p1, p2))))
+#print(y_point(end_segment(make_segment(p1, p2))))
+
+
+#Q8:
+def midpoint_segment(segment):
+    start_point = start_segment(segment)
+    end_point = end_segment(segment)
+
+    start_point_x_coordinate = x_point(start_point)
+    start_point_y_coordinate = y_point(start_point)
+
+    end_point_x_coordinate = x_point(end_point)
+    end_point_y_coordinate = y_point(end_point)
+
+    mid_point_x_coordinate = (start_point_x_coordinate + end_point_x_coordinate) / 2
+    mid_point_y_coordinate = (start_point_y_coordinate + end_point_y_coordinate) / 2
+    
+    return make_point(mid_point_x_coordinate, mid_point_y_coordinate)
+    
+#for running public test case, do not delete!
+p1 = make_point(2, 3)
+p2 = make_point(5, 7)
+s = make_segment(p1, p2)
+
+#print(x_point(midpoint_segment(s)))
+#print(y_point(midpoint_segment(s)))
