4bd967418bd87ec84325c86bfb38d7e8b651d5e4
Gerald Khoo <khoo0030@e.ntu.edu.sg>
2019-03-27 22:12:40 +0800

    Work in progress - mission05

--- missions/mission05-template.py
+++ missions/mission05-template.py
@@ -12,9 +12,52 @@ from hi_graph import *
 # Task 1 #
 ##########
 
+##def connect_ends(curve1, curve2):
+##    curve_1_start_point = connect_rigidly(curve1, curve2)(0)
+
+##    curve_2_start_point = connect_rigidly(curve1, curve2)(0.50)
+##    curve_2_end_point = connect_rigidly(curve1, curve2)(1)
+##
+##    def connected_curve(t):
+##        if (t < 0.5):
+##            return connect_rigidly(curve1, curve2)(t)
+##        else:
+##            translate_x = x_of(curve_1_end_point) - x_of(curve_2_start_point)
+##            translate_y = y_of(curve_1_end_point) - y_of(curve_2_start_point)
+##            return translate(translate_x, translate_y)(connect_rigidly(curve1, curve2))(t)
+##        
+##    return connected_curve
+
 def connect_ends(curve1, curve2):
-    "your solution here!"
-    pass
+    curve_1_end_point = connect_rigidly(curve1, curve2)(0.499)
+    curve_2_start_point = connect_rigidly(curve1, curve2)(0.50)
+
+    
+    def connected_curve(t):
+        if (t < 0.5):
+            return connect_rigidly(curve1, curve2)(t)
+        else:
+            translate_x = x_of(curve_1_end_point) - x_of(curve_2_start_point)
+            translate_y = y_of(curve_1_end_point) - y_of(curve_2_start_point)
+            return translate(translate_x, translate_y)(connect_rigidly(curve1, curve2))(t)
+        
+    return connected_curve
+
+draw_connected_scaled(200, connect_ends(arc, unit_line))
+draw_connected_scaled(200, connect_ends(translate(5,5)(arc), translate(1,1)(unit_line)))
+
+##draw_connected_scaled(10, connect_ends(arc, unit_line))
+
+##draw_connected_scaled(200, arc)
+##draw_connected_scaled(200, unit_line)
+##draw_connected_scaled(200, connect_rigidly(arc, unit_line))
+
+
+
+##c = connect_rigidly(arc, unit_line)(0.25)
+##print(x_of(c))
+##print(y_of(c))
+
 
 ##########
 # Task 2 #
