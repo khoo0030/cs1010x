e34aa53dca22d384778a078fc798a9e6de5da05f
Gerald Khoo <khoo0030@e.ntu.edu.sg>
2019-03-31 19:42:02 +0800

    Finish mission05

--- missions/mission05-template.py
+++ missions/mission05-template.py
@@ -12,51 +12,26 @@ from hi_graph import *
 # Task 1 #
 ##########
 
-##def connect_ends(curve1, curve2):
-##    curve_1_start_point = connect_rigidly(curve1, curve2)(0)
-
-##    curve_2_start_point = connect_rigidly(curve1, curve2)(0.50)
-##    curve_2_end_point = connect_rigidly(curve1, curve2)(1)
-##
-##    def connected_curve(t):
-##        if (t < 0.5):
-##            return connect_rigidly(curve1, curve2)(t)
-##        else:
-##            translate_x = x_of(curve_1_end_point) - x_of(curve_2_start_point)
-##            translate_y = y_of(curve_1_end_point) - y_of(curve_2_start_point)
-##            return translate(translate_x, translate_y)(connect_rigidly(curve1, curve2))(t)
-##        
-##    return connected_curve
-
 def connect_ends(curve1, curve2):
-    curve_1_end_point = connect_rigidly(curve1, curve2)(0.499)
-    curve_2_start_point = connect_rigidly(curve1, curve2)(0.50)
-
-    
     def connected_curve(t):
-        if (t < 0.5):
-            return connect_rigidly(curve1, curve2)(t)
-        else:
-            translate_x = x_of(curve_1_end_point) - x_of(curve_2_start_point)
-            translate_y = y_of(curve_1_end_point) - y_of(curve_2_start_point)
-            return translate(translate_x, translate_y)(connect_rigidly(curve1, curve2))(t)
+        curve1_end_point = curve1(1)
+
+        curve2_start_point = curve2(0)
+
+        translate_x = x_of(curve1_end_point) - x_of(curve2_start_point)
+        translate_y = y_of(curve1_end_point) - y_of(curve2_start_point)
+
+        translated_curve2 = translate(translate_x, translate_y)(curve2)
         
+        return connect_rigidly(curve1, translated_curve2)(t)
     return connected_curve
 
-draw_connected_scaled(200, connect_ends(arc, unit_line))
-draw_connected_scaled(200, connect_ends(translate(5,5)(arc), translate(1,1)(unit_line)))
-
-##draw_connected_scaled(10, connect_ends(arc, unit_line))
+##draw_connected_scaled(200, connect_ends(arc, unit_line))
+##draw_connected_scaled(200, connect_ends(translate(5,5)(arc), translate(1,1)(unit_line)))
 
 ##draw_connected_scaled(200, arc)
 ##draw_connected_scaled(200, unit_line)
 ##draw_connected_scaled(200, connect_rigidly(arc, unit_line))
-
-
-
-##c = connect_rigidly(arc, unit_line)(0.25)
-##print(x_of(c))
-##print(y_of(c))
 
 
 ##########
@@ -64,8 +39,14 @@ draw_connected_scaled(200, connect_ends(translate(5,5)(arc), translate(1,1)(unit
 ##########
 
 def show_points_gosper(level, num_points, initial_curve):
-    "your solution here!"
-    pass
+    curve = repeated(gosperize, level)(initial_curve)
+    
+    squeezed_curve = squeeze_curve_to_rect(-0.5, -0.5, 1.5, 1.5)(curve)
+    
+    return draw_points(num_points, squeezed_curve)
+
+##show_points_gosper(7, 1000, arc)
+##show_points_gosper(5, 500, arc)
 
 ##########
 # Task 3 #
@@ -79,9 +60,15 @@ def your_gosper_curve_with_angle(level, angle_at_level):
 
 def your_gosperize_with_angle(theta):
     def inner_gosperize(curve_fn):
-        return put_in_standard_position(connect_ends("your solution here"))
+    
+        left_curve = rotate(theta)(curve_fn)
+        right_curve = rotate(-theta)(curve_fn)
+        
+        return put_in_standard_position(connect_ends(left_curve, right_curve))
     return inner_gosperize
 
 # testing
-# draw_connected(200, your_gosper_curve_with_angle(10, lambda lvl: pi/(2+lvl)))
-# draw_connected(200, your_gosper_curve_with_angle(5, lambda lvl: (pi/(2+lvl))/(pow(1.3, lvl))))
+draw_connected(200, your_gosper_curve_with_angle(10, lambda lvl: pi/(2+lvl)))
+draw_connected(200, your_gosper_curve_with_angle(5, lambda lvl: (pi/(2+lvl))/(pow(1.3, lvl))))
+
+##draw_connected_scaled(200, gosper_curve_with_angle (10 , lambda lvl : pi/(2+lvl)))
